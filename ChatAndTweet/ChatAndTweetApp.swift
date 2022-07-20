b7e369e2bcb17723a45a4549dd14412d5adb9e8e
junsai <junsai0530@gmail.com>
2022-07-20 21:30:13 +0900

    Initial Commit

---
+++ ChatAndTweet/ChatAndTweetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAndTweetApp.swift
+//  ChatAndTweet
+//
+//  Created by 小暮準才 on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatAndTweetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
