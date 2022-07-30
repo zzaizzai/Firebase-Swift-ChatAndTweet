e7106903a0b5772830bb2b101a458363fbdd794e
junsai <junsai0530@gmail.com>
2022-07-30 09:38:41 +0900

    main post view check didLike

--- ChatAndTweet/Model/Post.swift
+++ ChatAndTweet/Model/Post.swift
@@ -19,3 +19,29 @@ import FirebaseFirestoreSwift
 //
 //}
 //
+
+//struct MyLikedPost: Identifiable {
+//
+//    var id: String {documentId}
+//    let documentId: String
+//
+//    let authorUid, content : String
+//    let authorName, authorEmail : String
+//    let authorProfileUrl : String
+//    let date: Date
+//    var likes: Int
+//    var liked: Bool
+//
+//    init(documentId: String, data: [String:Any]) {
+//        self.documentId = documentId
+//
+//        self.content = data["content"] as? String ?? "no content"
+//        self.authorUid = data["authorId"] as? String ?? "no authorId"
+//        self.authorName = data["authorName"] as? String ?? "no authorName"
+//        self.authorEmail = data["authorEmail"] as? String ?? "no authorEmail"
+//        self.authorProfileUrl = data["authorProfileUrl"] as? String ?? "no authorProfileUrl"
+//        self.date = data["date"] as? Date ?? Date()
+//        self.likes = data["likes"] as? Int ?? 0
+//        self.liked = data["liked"] as? Bool ?? false
+//    }
+//}
